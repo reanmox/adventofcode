34a62dbeee3fe3ffa780e305f4b74a085d4d62c8
reanmox <47215768+reanmox@users.noreply.github.com>
2023-12-22 15:22:03 +0100

    adjust template

--- template.php
+++ template.php
@@ -3,19 +3,11 @@
 $filepath = "input/.txt";
 $result = [];
 
-// read input data
 $contents = file_get_contents($filepath);
-
-// split content on new line
 $contents = explode("\n", $contents);
 
 foreach ($contents as $index => $line) {
-    // do something with each line
-
-    // add to result
     $result[$index] = $line;
 }
 
 echo print_r($result, true);
-
-// room for helper functions
